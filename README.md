325379f83ab57c25b289885350109addbcfe5098
bomibear <bomibearly@gmail.com>
2019-06-05 12:56:49 -0700

    made alert comments DRY

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Friends!  Welcome!  Zoidberg is the best, so why not Zoidberg?!
 ## Overview
 This web application is a practical implementation of pure, unadulterated JavaScript.  Full of prompts and alerts, this application solicits answers from the user in regards to questions about Zoidberg.
 
+---
 ## Build
 Run the following commands in your terminal.  Choose a place to save the directory, Finally, run the application locally by opening it in your favorite browser.
 
@@ -20,6 +21,11 @@ cd AboutZoidberg
 open index.html
 ```
 
+---
+## Sprint Summaries
+1. Added input functionality for 5 questions
+
+2. Added 2 additional questions. The 6th is a randon number guessing question, and the 7th question involves the use of an array. Additionally, a tally of correct answers is being kept.
 ---
 ## Acknowledgements
 Shake animation was from [here](https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_image_shake).
--- js/app.js
+++ js/app.js
@@ -1,51 +1,54 @@
 'use strict';
 
+var correct = 'Correct!!!';
+var notCorrect = 'NO!!!';
+
 //Q1
 var hasDoctorate = prompt('Do I have a doctorate in Art History?').toLowerCase();
 if(hasDoctorate === 'yes' || hasDoctorate === 'y'){
-  alert('Why, yes.  I am quite the art connoisseur');
+  alert(correct);
 }
 if(hasDoctorate === 'no' || hasDoctorate === 'n'){
-  alert('I am quite the accomplished doctor, of the art history kind!!!');
+  alert(notCorrect);
 }
 console.log('Q1 asks if Zoidberg has an art history doctorate and the User said ' + hasDoctorate);
 
 //Q2
 var hasKids = prompt('Do I have kids?').toLowerCase();
 if(hasKids === 'yes' || hasKids === 'y'){
-  alert('Sadly, I have not found a mate of the same species :(');
+  alert(correct);
 }
 if(hasKids === 'no' || hasKids === 'n'){
-  alert('Zoidberg has no offspring!!!');
+  alert(notCorrect);
 }
 console.log('Q2 asks if Zoidberg has kids and the User said ' + hasKids);
 
 //Q3
 var hasHair = prompt('Do I have hair').toLowerCase();
 if(hasHair === 'no' || hasHair === 'n'){
-  alert('My exoskeleton is so nice and shiny!');
+  alert(correct);
 }
 if(hasHair === 'yes' || hasHair === 'y'){
-  alert('Only Zoidberg Jesus has hair!');
+  alert(notCorrect);
 }
 console.log('Q3 asks if Zoidberg hair and the User said ' + hasHair);
 
 //Q4
 var livesInSeattle = prompt('Do I live in Seattle?').toLowerCase();
 if(livesInSeattle === 'yes' || livesInSeattle === 'y'){
-  alert('That was so 2000 years ago.  Nope.');
+  alert(notCorrect);
 }
 if(livesInSeattle === 'no' || livesInSeattle === 'n'){
-  alert('New New York is the place to be!');
+  alert(correct);
 }
 console.log('Q4 asks if Zoidberg lives in Seattle and the User said ' + livesInSeattle);
 
 //Q5
 var deliveryCompany = prompt('Do I work for a delivery company?').toLowerCase();
 if(deliveryCompany === 'no' || deliveryCompany === 'n'){
-  alert('Being a distinguished doctor does not make Zoidberg enough money.');
+  alert(notCorrect);
 }
 if(deliveryCompany === 'yes' || deliveryCompany === 'y'){
-  alert('Zoidberg loves Planet Express!');
+  alert(correct);
 }
 console.log('Q5 asks if Zoidberg works for Planet Express and the User said ' + deliveryCompany);
